bc6a91f695faeb694d4ca460fbc663d4fcbf22e2
Perunaz <caelan.van.eijnsbergen@gmail.com>
2021-04-19 14:14:22 +0200

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const importData = require("./data.json");
-const hostname = '127.0.0.1'
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send(importData);
 })
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`)
   })
