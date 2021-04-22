301254fdcf85d0d8725207d7d7ae3876c627c73d
Perunaz <caelan.van.eijnsbergen@gmail.com>
2021-04-22 10:08:55 +0200

    added info page

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-const importData = require("./data.json");
+const importData1 = require("./data1.json");
+const importData2 = require("./data2.json");
 let port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send(importData);
-})
+app.get("/api", (req, res) => {
+    res.send(importData1);
+});
+app.get("/api/info", (req, res) => {
+    res.send(importData2);
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`)
-  })
+});
