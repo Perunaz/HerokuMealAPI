9663afc8235764bbf66d6c481ad08d121b2fb867
Caelan van Eijnsbergen <43774541+Perunaz@users.noreply.github.com>
2021-04-26 15:38:19 +0200

    seperated Api routes and functions

---
+++ controllers/api.controller.js
@@ -0,0 +1,39 @@
+const importData1 = require("../json/data1.json");
+const importData2 = require("../json/data2.json");
+const apiRoutes = require("../route/api.routes");
+
+exports.getLoggingHandler = function(req, res, next) {
+    console.log("Generic logging handler called.")
+    next();
+}, (req, res, next) => {
+    const reqMethod = req.method;
+    const reqUrl = req.url;
+    console.log("Endpoint called: " + reqMethod + " " + reqUrl);
+    next();
+};
+
+exports.getHelloWorld = function(req, res) {
+    res.send(importData1);
+    res.status(200).json(result);
+};
+
+exports.getApiInfo = function(req, res) {
+    console.log("ïnfo endpoint called");
+    res.send(importData2);
+    res.status(200).json(result);
+};
+
+exports.getCatchAll = function(req, res, next) {
+    console.log("Catch-all endpoint called");
+    next();
+};
+
+exports.getErrorOccurred = function(error, req, res, next) {
+    console.log("Errorhandler called!");
+    console.log(error);
+
+    res.status(500).json({
+        message: "Some error occurred",
+        error,
+    });
+}
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-require('./routes')(express);
 const database = require("./database");
 
 const app = express();
-const importData1 = require("./data1.json");
-const importData2 = require("./data2.json");
+require('./route/api.routes.js')(app);
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
