c5ae6e822ca9ad6ea9305d6e306b0d5de7f9a7cf
Caelan van Eijnsbergen <43774541+Perunaz@users.noreply.github.com>
2021-04-29 14:30:17 +0200

    hotfix 2.1

--- src/routes/api.homeroutes.js
+++ src/routes/api.homeroutes.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
-
     const api = require("../controllers/api.homecontroller.js");
+    const database = require("../dao/database.js");
 
     app.get("/api/studenthome/:homeId", api.getById);
 }
